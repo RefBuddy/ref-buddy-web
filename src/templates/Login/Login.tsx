6adff1ad49e247e46b6097cc89e5d5b50fa3fb95
jones <35585020+fleeced@users.noreply.github.com>
2023-06-16 17:20:11 -0600

    login connected to firebase

--- src/templates/Login/Login.tsx
+++ src/templates/Login/Login.tsx
@@ -1,10 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { auth } from '../../firebaseOptions';
+import { signInWithEmailAndPassword } from 'firebase/auth'; // Add this import
 
-// Create login component with firebase
 const Login: React.FC<any> = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password); // Change this line
+      console.log('Logged in successfully');
+      // Redirect to your desired page after successful login
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div>
       <h1>Login</h1>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
 }
