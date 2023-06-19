7fe74b1ee99765274def46ab65c9c26cfad2e12a
jones <35585020+fleeced@users.noreply.github.com>
2023-06-19 13:50:55 -0600

    game style in selectedGames is what we want

--- src/components/Calendar/SelectedGames/SelectedGames.tsx
+++ src/components/Calendar/SelectedGames/SelectedGames.tsx
@@ -8,21 +8,29 @@ const SelectedGames = () => {
   const clear = () => {
     dispatch(setSelectedGames([]));
   }
+  console.log(selectedGames)
   return (
     <div>
       <div className="flex flex-row justify-between items-center p-5">
         <h1>Selected Games</h1>
         <button onClick={() => clear()}>Clear</button>
       </div>
-      <div className="flex flex-row items-center justify-center gap-4 flex-wrap max-w-2/3">
+      <div className="flex flex-row items-center gap-4 flex-wrap max-w-2/3">
         {selectedGames.map(game => (
-          <div key={game.id} className="flex flex-col items-center justify-center gap-3 border-gray-200 border-solid border rounded shadow-sm p-5 cursor-pointer">
-            <div className={`flex flex-row items-center justify-center gap-3`}>
-              <img width={50} height={50} src={game.homeTeam.logo} alt="home team logo" />
-              <p>vs</p>
-              <img width={50} height={50} src={game.visitingTeam.logo} alt="visiting team logo" />
+          <div key={game.id} className="w-full flex flex-col items-start justify-center gap-3 border-gray-200 border-solid border rounded shadow-sm p-5 mx-4 cursor-pointer">
+            <div className={`flex flex-row items-center gap-3`}>
+              <div className="flex flex-col items-center justify-center">
+                <img width={70} height={70} src={game.visitingTeam.logo} alt="visiting team logo" />
+                <p className="text-sm opacity-70 pt-2">{game.visitingTeam.city}</p>
+                <p className="text-gray-700 text-sm opacity-50">Visiting</p>
+              </div>
+              <div style={{ minWidth: '30px' }}></div>
+              <div className="flex flex-col items-center justify-center">
+                <img width={70} height={70} src={game.homeTeam.logo} alt="home team logo" />
+                <p className="text-sm opacity-70 pt-2">{game.homeTeam.city}</p>
+                <p className="text-gray-700 text-sm opacity-50">Home</p>
+              </div>
             </div>
-            <p>{game.homeTeam.abbreviation} vs. {game.visitingTeam.abbreviation}</p>
           </div>
         ))}
       </div>
