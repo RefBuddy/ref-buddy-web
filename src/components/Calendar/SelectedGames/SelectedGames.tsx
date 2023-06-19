4abd923da312eb94af4bfb4ac1c323dba3891a25
jones <35585020+fleeced@users.noreply.github.com>
2023-06-19 14:28:40 -0600

    boxes for officials on right side of game

--- src/components/Calendar/SelectedGames/SelectedGames.tsx
+++ src/components/Calendar/SelectedGames/SelectedGames.tsx
@@ -2,6 +2,15 @@ import React from 'react';
 import { useAppDispatch, useAppSelector } from '../../../store';
 import { setSelectedGames } from '../../../store/Games/reducer';
 
+const Box = ({ imgSrc, altText, label }) => {
+  return (
+    <div className="flex flex-col items-center justify-center border p-2 mx-1">
+      <img width={70} height={70} src={imgSrc} alt={altText} />
+      <p className="text-sm opacity-70 pt-2">{label}</p>
+    </div>
+  );
+}
+
 const SelectedGames = () => {
   const dispatch = useAppDispatch();
   const selectedGames = useAppSelector(state => state.games.selectedGames)
@@ -18,7 +27,8 @@ const SelectedGames = () => {
       <div className="flex flex-row items-center gap-4 flex-wrap max-w-2/3">
         {selectedGames.map(game => (
           <div key={game.id} className="w-full flex flex-col items-start justify-center gap-3 border-gray-200 border-solid border rounded shadow-sm p-5 mx-4 cursor-pointer">
-            <div className={`flex flex-row items-center gap-3`}>
+          <div className={`flex flex-row items-start justify-between gap-3 w-full`}>
+            <div className="flex flex-row items-center gap-3">
               <div className="flex flex-col items-center justify-center">
                 <img width={70} height={70} src={game.visitingTeam.logo} alt="visiting team logo" />
                 <p className="text-sm opacity-70 pt-2">{game.visitingTeam.city}</p>
@@ -31,7 +41,14 @@ const SelectedGames = () => {
                 <p className="text-gray-700 text-sm opacity-50">Home</p>
               </div>
             </div>
+            <div className="flex">
+              <Box imgSrc="https://via.placeholder.com/70" altText="Referee 1" label="Referee 1" />
+              <Box imgSrc="https://via.placeholder.com/70" altText="Referee 2" label="Referee 2" />
+              <Box imgSrc="https://via.placeholder.com/70" altText="Linesman 1" label="Linesman 1" />
+              <Box imgSrc="https://via.placeholder.com/70" altText="Linesman 2" label="Linesman 2" />
+            </div>
           </div>
+        </div>        
         ))}
       </div>
     </div>
