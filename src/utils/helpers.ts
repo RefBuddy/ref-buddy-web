e0ba28cd3fdc377763bacaa571c1bd31d57da8c7
jones <35585020+fleeced@users.noreply.github.com>
2023-06-20 19:10:13 -0600

    added formatTime helper

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -4,6 +4,15 @@ const formatDate = (date: Date): string => {
   return format(date, 'yyyy-MM-dd');
 }
 
+const formatTime = (time: string): string => {
+  const date = new Date(time);
+  const hours = date.getHours() % 12 || 12;
+  const minutes = date.getMinutes();
+  const ampm = date.getHours() >= 12 ? 'PM' : 'AM';
+  return `${hours}:${minutes < 10 ? '0' : ''}${minutes} ${ampm}`;
+}
+
 export {
-  formatDate
+  formatDate,
+  formatTime,
 }
