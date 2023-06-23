6493d4716d01bb8abc1757df59e9e194b5ed0ab1
jones <35585020+fleeced@users.noreply.github.com>
2023-06-23 15:36:17 -0600

    added overview travel component to dashboard

---
+++ src/components/OverviewTravel/OverviewTravel.tsx
@@ -0,0 +1,124 @@
+import React, { FC } from 'react';
+import { Typography, Box, SvgIcon, useTheme } from '@mui/material';
+import { Chart } from '../Chart';
+
+interface OverviewTravelProps {
+  chartSeries: number[];
+  labels: string[];
+  sx?: object;
+}
+
+const useChartOptions = (labels: string[]) => {
+  const theme = useTheme();
+
+  return {
+    chart: {
+      background: 'transparent'
+    },
+    colors: [
+      '#f5a614', // orange
+      '#000000' // black
+    ],
+    dataLabels: {
+      enabled: false
+    },
+    labels,
+    legend: {
+      show: false
+    },
+    plotOptions: {
+      pie: {
+        expandOnClick: false
+      }
+    },
+    states: {
+      active: {
+        filter: {
+          type: 'none'
+        }
+      },
+      hover: {
+        filter: {
+          type: 'none'
+        }
+      }
+    },
+    stroke: {
+      width: 0
+    },
+    theme: {
+      mode: theme.palette.mode
+    },
+    tooltip: {
+      fillSeriesColor: false
+    }
+  };
+};
+
+const OverviewTravel: FC<OverviewTravelProps> = ({ chartSeries, labels, sx }) => {
+  const chartOptions = useChartOptions(labels);
+
+  return (
+    <div className="flex items-center justify-center gap-3 border-gray-200 border-solid border rounded-lg shadow-sm px-4 mx-4" style={{ width: '300px' }}>
+      <div className="flex flex-1 flex-col items-center justify-center gap-3">
+        <Typography
+          color="text.secondary"
+          variant="overline"
+          style={{ alignSelf: 'flex-start', paddingTop: '1rem' }}
+        >
+          Travel
+        </Typography>
+        <div className="flex items-center justify-center w-full">
+          <Chart
+            height={250}
+            options={chartOptions}
+            series={chartSeries}
+            type="donut"
+            width="80%"
+          />
+        </div>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            gap: 2,
+            mt: 2
+          }}
+        >
+          {chartSeries.map((item, index) => {
+            const label = labels[index];
+
+            return (
+              <Box
+                key={label}
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  alignItems: 'center'
+                }}
+              >
+                <Typography
+                  sx={{ my: 1 }}
+                  variant="h6"
+                >
+                  {label}
+                </Typography>
+                <Typography
+                  color="text.secondary"
+                  variant="subtitle2"
+                  style={{ paddingBottom: '1rem' }}
+                >
+                  {item}
+                </Typography>
+              </Box>
+            );
+          })}
+        </Box>
+      </div>
+    </div>
+  );
+};
+
+export default OverviewTravel;
--- src/templates/Dashboard/Dashboard.tsx
+++ src/templates/Dashboard/Dashboard.tsx
@@ -9,6 +9,7 @@ import { SelectedGames } from '../../components/Calendar/SelectedGames';
 import { Loading } from '../../components/Loading';
 import { OverviewBudget } from '../../components/OverviewBudget';
 import { OverviewGameReports } from '../../components/OverviewGameReports';
+import { OverviewTravel } from '../../components/OverviewTravel';
 
 const Dashboard: React.FC<any> = () => {
   const openModal = useAppSelector(state => state.modal.modalOpen);
@@ -16,6 +17,11 @@ const Dashboard: React.FC<any> = () => {
   const selectedEvent = useAppSelector(state => state.games.selectedEvent);
   const selectedGames = useAppSelector(state => state.games.selectedGames);
   const loading = useAppSelector(state => state.games.loading);
+
+  // Here's an example of data that you might pass to the OverviewTravel component.
+  // You'll want to replace this with actual data.
+  const chartSeries = [3, 6];
+  const labels = ['Hotel', 'Home'];
 
   return (
     <div style={{ display: 'flex' }}>
@@ -26,7 +32,12 @@ const Dashboard: React.FC<any> = () => {
           <OverviewBudget value="$10,000" positive={true} difference={10} />
           <OverviewGameReports progress={44} value="4 / 9" />
         </div>
-        {selectedGames && selectedGames.length === 0 ? <MyCalendar /> : <></>}
+        <div className="flex items-center p-5 justify-between">
+          <div className="flex-1">
+            {selectedGames && selectedGames.length === 0 && <MyCalendar />}
+          </div>
+          <OverviewTravel chartSeries={chartSeries} labels={labels} />
+        </div>
         {selectedGames && selectedGames.length > 0 ? <SelectedGames /> : <></>}
         {openModal && modalType === 'event' && selectedEvent && (
           createPortal(
