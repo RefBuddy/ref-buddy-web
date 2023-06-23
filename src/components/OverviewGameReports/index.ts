9e7adb90d4222bc21a75f03daaedc2d6a076402a
jones <35585020+fleeced@users.noreply.github.com>
2023-06-23 14:24:26 -0600

    added game reports component to dashboard

---
+++ src/components/OverviewGameReports/index.ts
@@ -0,0 +1,1 @@
+export { default as OverviewGameReports } from './OverviewGameReports';
--- src/templates/Dashboard/Dashboard.tsx
+++ src/templates/Dashboard/Dashboard.tsx
@@ -8,6 +8,7 @@ import { EventModal } from '../../components/Calendar/EventModal';
 import { SelectedGames } from '../../components/Calendar/SelectedGames';
 import { Loading } from '../../components/Loading';
 import { OverviewBudget } from '../../components/OverviewBudget';
+import { OverviewGameReports } from '../../components/OverviewGameReports';
 
 const Dashboard: React.FC<any> = () => {
   const openModal = useAppSelector(state => state.modal.modalOpen);
@@ -21,8 +22,9 @@ const Dashboard: React.FC<any> = () => {
       <Navbar />
       <main style={{ flex: 1 }}>
         {loading ? <Loading /> : <></>}
-        <div className="flex flex-row justify-between items-center p-5">
+        <div className="flex items-center p-5">
           <OverviewBudget value="$10,000" positive={true} difference={10} />
+          <OverviewGameReports progress={44} value="4 / 9" />
         </div>
         {selectedGames && selectedGames.length === 0 ? <MyCalendar /> : <></>}
         {selectedGames && selectedGames.length > 0 ? <SelectedGames /> : <></>}
