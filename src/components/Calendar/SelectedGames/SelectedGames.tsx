e4705f082f270ca20bbf4f8c3cc58972a4e565da
jones <35585020+fleeced@users.noreply.github.com>
2023-06-30 16:53:52 -0600

    game assignment shows officials in correct roles

--- src/components/Calendar/SelectedGames/SelectedGames.tsx
+++ src/components/Calendar/SelectedGames/SelectedGames.tsx
@@ -63,16 +63,32 @@ const OfficialBox = ({ official, game, role, label, color }) => {
 };
 
 const GameAssignment = ({ officialsData, gameData }) => {
+  // Create a new object to store the officials by their role
+  const officialsByRole = {};
+
+  // Loop through game officials
+  for (let gameOfficial of gameData.officials) {
+    // Loop through officialsData
+    for (let officialKey in officialsData) {
+      const official = officialsData[officialKey];
+      // Check if uid matches
+      if (official && official.uid === gameOfficial.uid) {
+        officialsByRole[gameOfficial.role] = official;
+        break;
+      }
+    }
+  }
 
   return (
     <div className="flex">
-      <OfficialBox official={officialsData.official1} game={gameData} role="referee1" label="Referee" color="orange" />
-      <OfficialBox official={officialsData.official2} game={gameData} role="referee2" label="Referee" color="orange" />
-      <OfficialBox official={officialsData.official3} game={gameData} role="linesman1" label="Linesman" color="black" />
-      <OfficialBox official={officialsData.official4} game={gameData} role="linesman2" label="Linesman" color="black" />
+      <OfficialBox official={officialsByRole["referee1"]} game={gameData} role="referee1" label="Referee" color="orange" />
+      <OfficialBox official={officialsByRole["referee2"]} game={gameData} role="referee2" label="Referee" color="orange" />
+      <OfficialBox official={officialsByRole["linesman1"]} game={gameData} role="linesman1" label="Linesman" color="black" />
+      <OfficialBox official={officialsByRole["linesman2"]} game={gameData} role="linesman2" label="Linesman" color="black" />
     </div>
   );
 };
+
 
 const SelectedGames = () => {
   const dispatch = useAppDispatch();
