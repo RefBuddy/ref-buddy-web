e4110b129490adfcfe11b5f30c91050b098aaf87
jones <35585020+fleeced@users.noreply.github.com>
2023-07-05 13:20:04 -0600

    constants folder with team names

---
+++ src/constants/TeamNames.ts
@@ -0,0 +1,19 @@
+const teamNames: string[] = [
+    "Alberni Valley Bulldogs",
+    "Chilliwack Chiefs",
+    "Coquitlam Express",
+    "Cranbrook Bucks",
+    "Cowichan Valley Capitals",
+    "Langley Rivermen",
+    "Merritt Centennials",
+    "Nanaimo Clippers",
+    "Penticton Vees",
+    "Powell River Kings",
+    "Prince George Spruce Kings",
+    "Salmon Arm Silverbacks",
+    "Surrey Eagles",
+    "Trail Smoke Eaters",
+    "Vernon Vipers",
+    "Victoria Grizzlies",
+    "West Kelowna Warriors"
+];
