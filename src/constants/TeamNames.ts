73787558b99b7122025d3770f0fc90d21d84e12a
jones <35585020+fleeced@users.noreply.github.com>
2023-07-05 14:30:17 -0600

    added team info

--- src/constants/TeamNames.ts
+++ src/constants/TeamNames.ts
@@ -1,19 +1,189 @@
-const teamNames: string[] = [
-    "Alberni Valley Bulldogs",
-    "Chilliwack Chiefs",
-    "Coquitlam Express",
-    "Cranbrook Bucks",
-    "Cowichan Valley Capitals",
-    "Langley Rivermen",
-    "Merritt Centennials",
-    "Nanaimo Clippers",
-    "Penticton Vees",
-    "Powell River Kings",
-    "Prince George Spruce Kings",
-    "Salmon Arm Silverbacks",
-    "Surrey Eagles",
-    "Trail Smoke Eaters",
-    "Vernon Vipers",
-    "Victoria Grizzlies",
-    "West Kelowna Warriors"
-];
+const teamNames: {} = {
+    "Alberni Valley Bulldogs": {
+        abbreviation: "AV",
+        city: "Alberni Valley",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/16.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Alberni Valley Bulldogs"
+    },
+    "Chilliwack Chiefs": {
+        abbreviation: "CHW",
+        city: "Chilliwack",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/13.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Chilliwack Chiefs"
+    },
+    "Coquitlam Express": {
+        abbreviation: "COQ",
+        city: "Coquitlam",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/9.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Coquitlam Express"
+    },
+    "Cranbrook Bucks": {
+        abbreviation: "CRA",
+        city: "Cranbrook",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/39.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Cranbrook Bucks"
+    },
+    "Cowichan Valley Capitals": {
+        abbreviation: "CV",
+        city: "Cowichan Valley",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/10.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Cowichan Valley Capitals"
+    },
+    "Langley Rivermen": {
+        abbreviation: "LAN",
+        city: "Langley",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/11.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Langley Rivermen"
+    },
+    "Merritt Centennials": {
+        abbreviation: "MER",
+        city: "Merritt",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/15.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Merritt Centennials"
+    },
+    "Nanaimo Clippers": {
+        abbreviation: "NAN",
+        city: "Nanaimo",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/23.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Nanaimo Clippers"
+    },
+    "Penticton Vees": {
+        abbreviation: "PEN",
+        city: "Penticton",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/12.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Penticton Vees"
+    },
+    "Powell River Kings": {
+        abbreviation: "PR",
+        city: "Powell River",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/8.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Powell River Kings"
+    },
+    "Prince George Spruce Kings": {
+        abbreviation: "PG",
+        city: "Prince George",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/14.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Prince George Spruce Kings"
+    },
+    "Salmon Arm Silverbacks": {
+        abbreviation: "SA",
+        city: "Salmon Arm",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/20.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Salmon Arm Silverbacks"
+    },
+    "Surrey Eagles": {
+        abbreviation: "SUR",
+        city: "Surrey",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/11.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Surrey Eagles"
+    },
+    "Trail Smoke Eaters": {
+        abbreviation: "TRA",
+        city: "Trail",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/22.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Trail Smoke Eaters"
+    },
+    "Vernon Vipers": {
+        abbreviation: "VER",
+        city: "Vernon",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/21.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Vernon Vipers"
+    },
+    "Victoria Grizzlies": {
+        abbreviation: "VIC",
+        city: "Victoria",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/18.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "Victoria Grizzlies"
+    },
+    "West Kelowna Warriors": {
+        abbreviation: "WK",
+        city: "West Kelowna",
+        goals: 0,
+        logo: "https://assets.leaguestat.com/bchl/logos/19.jpg",
+        penalties: 0,
+        penalty_minutes: 0,
+        powerplay_goals: 0,
+        shots: 0,
+        team_name: "West Kelowna Warriors"
+    }
+};
