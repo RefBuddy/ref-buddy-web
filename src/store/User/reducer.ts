a626c3564aa1b3e11b45895abcaf70792ac45859
jones <35585020+fleeced@users.noreply.github.com>
2023-08-17 11:16:36 -0600

    remove console logs

--- src/store/User/reducer.ts
+++ src/store/User/reducer.ts
@@ -38,7 +38,6 @@ const userSlice = createSlice({
       state.currentSeason = payload;
     },
     setCurrentLeague: (state, { payload }) => {
-      console.log('payload', payload);
       state.currentLeague = payload;
     },
   },
@@ -47,7 +46,6 @@ const userSlice = createSlice({
       state.loading = true;
     });
     builder.addCase(getUserCalendarEvents.fulfilled, (state, { payload }) => {
-      console.log('payload', payload);
       state.assignedGames = payload.assignedGames;
       state.queuedGames = payload.queuedGames;
       state.blockedOffTimes = payload.blockedOffTimes;
@@ -76,8 +74,6 @@ const userSlice = createSlice({
     builder.addCase(
       getAllOfficialsCalendarEvents.fulfilled,
       (state, { payload }) => {
-        console.log('hiiii');
-        console.log('payload', payload);
         state.officialsCalendarData = payload;
         state.error = false;
         state.loading = false;
@@ -97,7 +93,6 @@ const userSlice = createSlice({
       state.currentLeague = payload.leagues[0];
       state.error = false;
       state.loading = false;
-      console.log('state.currentLeague', state.currentLeague);
     });
     builder.addCase(getUserLeagues.rejected, (state, { error }) => {
       state.error = error;
