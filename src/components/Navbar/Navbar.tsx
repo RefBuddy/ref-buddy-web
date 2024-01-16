b5c24ef6b64182560b47f670b5c368b88321690e
Jonah <duckwxrth@gmail.com>
2024-01-16 11:52:09 -0700

    chore: navbar styling and only bchl can see links

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAppSelector } from '../../store';
 import { signOut } from 'firebase/auth';
 import { Link } from 'gatsby';
 import { auth } from '../../firebaseOptions';
@@ -6,6 +7,8 @@ import Logo from '../../images/favicon.png';
 
 const Navbar: React.FC<any> = () => {
   const uid = auth.currentUser?.uid;
+  const user = useAppSelector((state) => state.user);
+
   const handleLogout = async () => {
     try {
       await signOut(auth);
@@ -16,23 +19,37 @@ const Navbar: React.FC<any> = () => {
   };
 
   return (
-    <div className="flex h-screen sticky top-0 h-screen">
-      <nav className="flex flex-col justify-between items-center p-4 bg-black min-w-200px text-white">
+    <div className="flex h-screen sticky top-0">
+      <nav className="flex flex-col justify-start items-center p-4 bg-black min-w-200px text-white">
         <div className="flex items-center">
-          <img src={Logo} alt="logo" className="h-12 w-12 mr-2" />
+          <img src={Logo} alt="logo" className="h-16 w-16 mr-2" />
         </div>
-        <div className="flex flex-col text-sm">
-          <Link to={`/portal/${uid}/dashboard`}>Dashboard</Link>
-          <Link to={`/portal/${uid}/stats`}>Stats</Link>
-        </div>
+        {user.currentLeague === 'bchl' && (
+          <div className="flex flex-col text-lg mt-8 gap-2">
+            <Link
+              id="nav-link"
+              className="text-white hover:text-orange-500 link-focus"
+              to={`/portal/${uid}/dashboard`}
+            >
+              Dashboard
+            </Link>
+            <Link
+              id="nav-link"
+              className="text-white hover:text-orange-500 link-focus"
+              to={`/portal/${uid}/stats`}
+            >
+              Stats
+            </Link>
+          </div>
+        )}
         <button
           onClick={handleLogout}
-          className="border-white border bg-white text-black cursor-pointer px-1"
+          className="border-white border bg-white text-black cursor-pointer px-1 mt-auto"
         >
           Logout
         </button>
       </nav>
-      <div className="flex-grow bg-white">{/* Add your content here */}</div>
+      {/* <div className="flex-grow bg-white">Add your content here</div> */}
     </div>
   );
 };
