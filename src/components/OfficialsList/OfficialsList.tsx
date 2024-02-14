dcd1f8bc85bfec1394dd379c1c5e0177683b3d32
Jonah <duckwxrth@gmail.com>
2024-02-14 13:54:44 -0700

    add: components subdirectory and moved GameDetails to it

--- src/components/OfficialsList/OfficialsList.tsx
+++ src/components/OfficialsList/OfficialsList.tsx
@@ -4,6 +4,7 @@ import {  format24HourTime } from '../../utils/helpers';
 import { Button } from '../Button';
 import { ExclamationTriangleIcon } from '@heroicons/react/24/solid';
 import * as Utils from './utils';
+import { GameDetails } from './components';
 
 const OfficialsList = ({ game, role, isAssigned, close = () => {} }) => {
   const dispatch = useAppDispatch();
@@ -80,52 +81,7 @@ const OfficialsList = ({ game, role, isAssigned, close = () => {} }) => {
       {/* teams, game time, checkboxes */}
       <div className="flex items-center justify-between w-full -mt-6 -mb-3">
         {role != 'dashboard' ? (
-          <div className="flex w-full items-center justify-start p-4">
-            <div className="flex flex-row items-center gap-2 ml-6">
-              {currentLeague == 'bchl' ? (
-                <>
-                  <div className="flex flex-col items-center justify-center">
-                    <img
-                      width={40}
-                      height={40}
-                      src={game.visitingTeam.logo}
-                      alt="visiting team logo"
-                    />
-                    <p className="text-sm text-black text-center min-w-24">
-                      {game.visitingTeam.city}
-                    </p>
-                  </div>
-                  <div className="flex flex-col items-center justify-center">
-                    <div className="text-lg font-bold -mt-3">@</div>
-                  </div>
-                  <div className="flex flex-col items-center justify-center">
-                    <img
-                      width={40}
-                      height={40}
-                      src={game.homeTeam.logo}
-                      alt="home team logo"
-                    />
-                    <p className="text-sm text-black text-center min-w-24">
-                      {game.homeTeam.city}
-                    </p>
-                  </div>
-                </>
-              ) : (
-                <div className="w-full h-6 bg-white"></div>
-              )}
-              <div className="flex flex-col -mb-2">
-                <div className="text-xs font-semibold text-gray-700 mb-2 -mt-1 w-40">
-                  {Utils.getFormattedTime(game.time)}
-                </div>
-                <div className="text-xs font-semibold text-gray-700 mb-2 -mt-1">
-                  {game.date.slice(0, -6)}
-                </div>
-                <div className="text-xs font-semibold text-gray-700 mb-2 -mt-1">
-                  {game.venue}
-                </div>
-              </div>
-            </div>
-          </div>
+          <GameDetails game={game} currentLeague={currentLeague} />
         ) : (
           <div className="w-full h-6 bg-white"></div>
         )}
